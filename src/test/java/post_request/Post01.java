9f338fc2599951790f781ecb9b36153fca5c286b
ramazanuckun <rru.3206@gmail.com>
2022-11-04 10:58:04 +0300

    03.11.2022

--- src/test/java/post_request/Post01.java
+++ src/test/java/post_request/Post01.java
@@ -1,10 +1,16 @@
 package post_request;
 
 import base_urls.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 import test_data.JsonPlaceHolderTestData;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Post01 extends JsonplaceholderBaseUrl {
 
@@ -44,8 +50,23 @@ public class Post01 extends JsonplaceholderBaseUrl {
    Map<String,Object>expectedData=     obj.expectedDataMethod(55,"Tidy your room",false);
 
 
+   //set the request and the response
+
+        //postmanda ne yapiyorsam burada onu yaptim otel icin post diyerek yeni bir kullanici olustrdum
+
+
+      Response response= given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+   response.prettyPrint();
    //Do aseertion
 
+
+        Map<String,Object>actualData=response.as(HashMap.class);
+
+
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
 
 
 
